7cd8238005229d971c371fe74f590725500cae62
yokohama_tomoki@lightcafe@co.jp <vbitb4ow>
2023-12-15 11:49:43 +0900

    非該当レコードの追加

--- src/test/java/com/example/demo/PostalRecordFinderTest.java
+++ src/test/java/com/example/demo/PostalRecordFinderTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
@@ -20,12 +19,14 @@ public class PostalRecordFinderTest {
     private final File file1 = mock(File.class);
     private final File file2 = mock(File.class);
     private final File file3 = mock(File.class);
+    private final File file4 = mock(File.class);
 
-    private final  File[] files = {file1,file2,file3};
+    private final  File[] files = {file1,file2,file3,file4};
 
     private final CSVRecord record1 = mock(CSVRecord.class);
     private final CSVRecord record2 = mock(CSVRecord.class);
     private final CSVRecord record3 = mock(CSVRecord.class);
+    private final CSVRecord record4 = mock(CSVRecord.class);
     {
         doReturn("アサクサ").when(record1).get(5);
         doReturn("東京都").when(record1).get(6);
@@ -41,6 +42,11 @@ public class PostalRecordFinderTest {
         doReturn("熊本県").when(record3).get(6);
         doReturn("八代市").when(record3).get(7);
         doReturn("旭中央通").when(record3).get(8);
+
+        doReturn("イカニケイサイガナイバアイ").when(record4).get(5);
+        doReturn("埼玉県").when(record4).get(6);
+        doReturn("川越市").when(record4).get(7);
+        doReturn("以下に掲載がない場合").when(record4).get(8);
     }
 
     private final PostalRecord postalRecord1 =
@@ -52,12 +58,11 @@ public class PostalRecordFinderTest {
     private final PostalRecord postalRecord3 =
             new PostalRecord(record3.get(5),record3.get(6),record3.get(7),record3.get(8));
 
+    private final PostalRecord postalRecord4 =
+            new PostalRecord(record4.get(5),record4.get(6),record4.get(7),record4.get(8));
+
     private final List<PostalRecord> postalRecords = List.of(postalRecord1,postalRecord2,postalRecord3);
 
-    private final PostalRecordKeywordMatchPredicate predicate = mock(PostalRecordKeywordMatchPredicate.class);
-    {
-        doReturn(true).when(predicate).test(any());
-    }
 
     private final FileStream fileStream = mock(FileStream.class);
     {
@@ -69,6 +74,7 @@ public class PostalRecordFinderTest {
         doReturn(record1).when(fileToCsvRecordFunction).apply(file1);
         doReturn(record2).when(fileToCsvRecordFunction).apply(file2);
         doReturn(record3).when(fileToCsvRecordFunction).apply(file3);
+        doReturn(record4).when(fileToCsvRecordFunction).apply(file4);
     }
 
     private final CsvRecordToPostalRecordFunction csvRecordToPostalRecordFunction = mock(CsvRecordToPostalRecordFunction.class);
@@ -76,6 +82,7 @@ public class PostalRecordFinderTest {
         doReturn(postalRecord1).when(csvRecordToPostalRecordFunction).apply(record1);
         doReturn(postalRecord2).when(csvRecordToPostalRecordFunction).apply(record2);
         doReturn(postalRecord3).when(csvRecordToPostalRecordFunction).apply(record3);
+        doReturn(postalRecord4).when(csvRecordToPostalRecordFunction).apply(record4);
     }
 
     private final PostalRecordFinder target = new PostalRecordFinder(
@@ -95,10 +102,12 @@ public class PostalRecordFinderTest {
             verify(fileToCsvRecordFunction).apply(file1);
             verify(fileToCsvRecordFunction).apply(file2);
             verify(fileToCsvRecordFunction).apply(file3);
+            verify(fileToCsvRecordFunction).apply(file4);
 
             verify(csvRecordToPostalRecordFunction).apply(record1);
             verify(csvRecordToPostalRecordFunction).apply(record2);
             verify(csvRecordToPostalRecordFunction).apply(record3);
+            verify(csvRecordToPostalRecordFunction).apply(record4);
         }
     }
 }
